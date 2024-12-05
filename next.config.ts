e8f79ae0c4ce38a356242f83cb43e6d667e799bd
itu-itis24-seyfullayev24 <seyfullayev24@itu.edu.tr>
2024-12-05 21:23:46 +0300

    fixed the policy for iframe content security

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Match all routes
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN", // Prevents your site from being embedded in iframes on different origins
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self';", // Allows only the same origin to embed the app
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
