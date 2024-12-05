3e0062f88ccc324e087d1961634d04541fbdfc9a
itu-itis24-seyfullayev24 <seyfullayev24@itu.edu.tr>
2024-12-05 21:10:14 +0300

    fixed post params error on props

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,17 +1,15 @@
 import { notFound } from "next/navigation";
-
+import { use } from "react";
 type Post = {
   slug: string;
   title: string;
   video: string;
   content: string;
 };
+type Params = Promise<{ slug: string }>;
 
-export default async function PostPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
+export default async function PostPage(props: { params: Params }) {
+  const params = use(props.params);
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`, {
     next: { revalidate: 60 }, // optional caching if needed
   });
